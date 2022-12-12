a30b441e2d54600760fa8431848eb355cba0583d
karimSATPRO <107763973+karimSATPRO@users.noreply.github.com>
2022-12-12 03:05:26 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,6 +6,8 @@ echo "############ OTEX #################"
 wget -O /usr/bin/RevCam "https://github.com/karimSATPRO/Revcamemu/blob/main/RevCam?raw=true"
 
 wget -O /etc/init.d/softcam.RevCamV2 "https://raw.githubusercontent.com/karimSATPRO/Revcamemu/main/softcam.RevCamV2"
+chmod 755 /usr/bin/revcamv2
+chmod 755 /etc/init.d/softcam.RevCamV2
 
 echo ""
 cd ..
