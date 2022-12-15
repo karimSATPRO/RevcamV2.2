350c0aa480b06e48dddbd961c992b826ad4afda5
karimSATPRO <107763973+karimSATPRO@users.noreply.github.com>
2022-12-15 17:05:26 +0100

    Update RevCamV2.sh

--- RevCamV2.sh
+++ RevCamV2.sh
@@ -1,18 +1,32 @@
-echo "##############################################"
-écho "#############################################" 
-echo "##############################################"
-echo "############ (RevcamV2) #################"
+echo ""
+echo ""
+echo "---------------------------------------------------------"
+echo "* RevCam Config & Softcam files in [/etc/tuxbox/revcam] *"
+echo "---------------------------------------------------------"
+echo ""
+echo ""
+echo "*******************************************"
+echo "*                                         *"
+echo "*                                         *"
+echo "*             Install Finished            *"
+echo "*                                         *"
+echo "*         Need To Restart Enigma2         *"
+echo "*                                         *"
+echo "*******************************************"
 
 wget -O /tmp/enigma2-plugin-softcams-revcam_2.0_all.ipk "https://github.com/karimSATPRO/RevcamV2.2/blob/main/enigma2-plugin-softcams-revcam_2.0_all.ipk?raw=true"
 
 opkg install --force-overwrite /tmp/*.ipk
 
-echo ""
-cd ..
-sync
-echo "############ Installation Successful  ###############"
-echo "############ Restart Enigma2 GUI... #################" 
-init 4
-sleep 2
-init 3
+#### Delete IPK File ####
+for file in /tmp/enigma2-plugin-softcams-revcam*.ipk; do
+    if [ -f "$file" ]; then
+        rm -rf /tmp/enigma2-plugin-softcams-revcam*.ipk
+        sleep 3
+    fi
+done
+
+#### Restart Image ####
+killall -9 enigma2
+
 exit 0
